3677d9140a68cd8fac270224cc40790ff2e13485
DK <startdks@gmail.com>
2026-01-10 23:48:06 -0500

    feat: enhance footer and navbar components with new layout and styles

--- client/src/app/components/footer/footer.ts
+++ client/src/app/components/footer/footer.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-footer',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './footer.html',
     styleUrl: './footer.scss',
 })
 export class FooterComponent {
-
+    smoothScroll(target: string): void {
+        const element = document.querySelector(target);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    }
 }
