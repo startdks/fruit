201d807fd759d7dc8599df3c847823d9415e2404
DK <startdks@gmail.com>
2026-01-14 23:53:59 -0500

    refactor: Update AppComponent test setup to include HTTP client and router providers and rename the describe block.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,19 +1,22 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
-
-describe('App', () => {
+describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [AppComponent],
-            providers: [provideZonelessChangeDetection()]
+            providers: [
+                provideZonelessChangeDetection(),
+                provideHttpClient(),
+                provideRouter([])
+            ]
         }).compileComponents();
     });
-
     it('should create the app', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
-
 });
